8a53efa075a66eb5a3f1dd81044778b8d65b0499
Nicolas Dao <nic@neap.co>
2019-05-27 08:51:52 +1000

    Update idpHelp.js

--- src/utils/idpHelp.js
+++ src/utils/idpHelp.js
@@ -11,20 +11,41 @@ const { join } = require('path')
 const urlHelp = require('./url')
 const fetch = require('./fetch')
 const { send, addErrorToUrl, defaultErrorMessage } = require('./response')
-
+	
 /**
- * Build a new URI made of the current request domain, a pathname, and optional request paramaters 'successRedirectUrl', 'errorRedirectUrl' 
- * @param  {Request} req      	Request object. 
- * @param  {String}  pathname 	Custom pathname
+ * Build a new URI made of the current request domain, a pathname, and optional request paramaters 
+ * 'successRedirectUrl', 'errorRedirectUrl', or 'success', 'failed'. The difference between the combo
+ * 'successRedirectUrl'/'errorRedirectUrl' and 'success'/'failed' is that the first one are URIs string encoded
+ * in the pathname while the last one are URIs passed in teh query params. The 2 can be mixed.
+ * 
+ * @param  {Request} req      							Request object. 
+ * @param  {String}  pathname 							Custom pathname
+ * @param  {Boolean} options.redirectsWithQueryParams 	Default false. If true, the onSuccess and onError URIs are passed
+ *														as query parameters rather than encoded paths. This is because some
+ *														IdPs fail to support redirects that contain encoded string in their pathname.
  * @return {String}  			New URI
  */
-const getCallbackUrl = (req, pathname) => {
-	const { successRedirectUrl, errorRedirectUrl } = req.params || {}
-	return urlHelp.buildUrl({ 
+const getCallbackUrl = (req, pathname, options) => {
+	const { redirectsWithQueryParams } = options || {}
+	const { successRedirectUrl, errorRedirectUrl, success, failed } = req.params || {}
+	const onSuccessURI = successRedirectUrl || success
+	const onErrorURI = errorRedirectUrl || failed
+	let urlParams = { 
 		protocol: req.secure ? 'https:' : 'http:', 
-		host: req.headers.host, 
-		pathname: (successRedirectUrl && errorRedirectUrl) ? join(pathname, successRedirectUrl, errorRedirectUrl) : pathname
-	})
+		host: req.headers.host
+	}
+
+	if (redirectsWithQueryParams) {
+		if (onSuccessURI && onErrorURI) 
+			urlParams.query = {
+				success:decodeURIComponent(onSuccessURI),
+				failed:decodeURIComponent(onErrorURI)
+			}
+		urlParams.pathname = pathname
+	} else 
+		urlParams.pathname = (successRedirectUrl && errorRedirectUrl) ? join(pathname, successRedirectUrl, errorRedirectUrl) : pathname
+
+	return urlHelp.buildUrl(urlParams, { noEncoding:true })
 }
 
 const authToUserPortal = ({ user, userPortal, strategy, successRedirect, formatErrorUrl, res, next }) => {
@@ -51,7 +72,7 @@ const authToUserPortal = ({ user, userPortal, strategy, successRedirect, formatE
 				else {
 					const redirectUrl = formatErrorUrl({
 						code:400, 
-						message: defaultErrorMessage,
+						message: `${defaultErrorMessage} (code 001)`,
 						verboseMessage: `The ${strategy} OAuth succeeded, HTTP GET to 'userPortal.api' ${userPortal.api} successed to but did not return a 'token' value ({ "token": null }).`,
 						data:user
 					})
@@ -61,7 +82,7 @@ const authToUserPortal = ({ user, userPortal, strategy, successRedirect, formatE
 				const errMsg = typeof(data) == 'string' ? data : data ? (data.message || (data.error || {}).message || JSON.stringify(data)) : null
 				const redirectUrl = formatErrorUrl({
 					code:400, 
-					message: errMsg || defaultErrorMessage,
+					message: errMsg || `${defaultErrorMessage} (code 002)`,
 					verboseMessage: `The ${strategy} OAuth succeeded, but HTTP GET to 'userPortal.api' ${userPortal.api} failed.${errMsg ? ` Details: ${errMsg}` : ''}`,
 					data:user
 				})
@@ -70,7 +91,7 @@ const authToUserPortal = ({ user, userPortal, strategy, successRedirect, formatE
 		}).catch(err => {
 			const redirectUrl = formatErrorUrl({
 				code: 500, 
-				message: defaultErrorMessage,
+				message: `${defaultErrorMessage} (code 003)`,
 				verboseMessage: err.message,
 				data: user
 			})
@@ -87,27 +108,32 @@ const authToUserPortal = ({ user, userPortal, strategy, successRedirect, formatE
  * Returns an Express handler that the client indirectly requests through the IdP redirection performed in response of 
  * an Authorization access request (done previously through the 'authRequestHandler' handler)
  *
- * @param  {String} strategy 			e.g., facebook, google, linkedin, github
- * @param  {String} userPortal.api 		URL of the API (POST) that accepts a user and returns a custom response.
- * @param  {String} userPortal.key   	API key used to safely communicate between 'UserIn' and the 'userPortal.api' endpoint.
- * @param  {Object} onSuccess   		Object defining a set of properties that are always returned in case of success
- * @param  {Object} onError   			Object defining a set of properties that are always returned in case of error
- * @param  {String} callbackPathname 	Only required for facebook. Otherwise, it throws a 'missing_redirect_uri' exception.
- * @return {Handler}           			[description]
+ * @param  {String} strategy 							e.g., facebook, google, linkedin, github
+ * @param  {String} userPortal.api 						URL of the API (POST) that accepts a user and returns a custom response.
+ * @param  {String} userPortal.key   					API key used to safely communicate between 'UserIn' and the 'userPortal.api' endpoint.
+ * @param  {Object} onSuccess   						Object defining a set of properties that are always returned in case of success
+ * @param  {Object} onError   							Object defining a set of properties that are always returned in case of error
+ * @param  {String} callbackPathname 					Only required for certain IdPs (e.g., facebook). Otherwise, it throws a 
+ *                                        				'missing_redirect_uri' exception.
+ * @param  {Boolean} options.redirectsWithQueryParams 	Default false. If true, the onSuccess and onError URIs are passed
+ *														as query parameters rather than encoded paths. This is because some
+ *														IdPs fail to support redirects that contain encoded string in their pathname.
+ *														
+ * @return {Handler}           							[description]
  */
-const getAuthResponseHandler = ({ strategy, userPortal, redirectUrls, callbackPathname }) => {
+const getAuthResponseHandler = ({ strategy, userPortal, redirectUrls, callbackPathname, redirectsWithQueryParams }) => {
 	const { onSuccess, onError } = redirectUrls || {}
 	const onErrorRedirectUrl = (onError || {}).default
 	const onSuccessRedirectUrl = (onSuccess || {}).default
 
 	return (req, res, next) => {
-		const { successRedirectUrl, errorRedirectUrl } = req.params || {}
-		const errorRedirect = decodeURIComponent(errorRedirectUrl || onErrorRedirectUrl)
-		const successRedirect = decodeURIComponent(successRedirectUrl || onSuccessRedirectUrl)
+		const { successRedirectUrl, errorRedirectUrl, success, failed } = req.params || {}
+		const errorRedirect = decodeURIComponent(failed || errorRedirectUrl || onErrorRedirectUrl)
+		const successRedirect = decodeURIComponent(success || successRedirectUrl || onSuccessRedirectUrl)
 		if (!errorRedirect) {
 			send(res, {
 				code:500,
-				message: defaultErrorMessage,
+				message: `${defaultErrorMessage} (code 004)`,
 				verboseMessage: 'Missing required redirect error url. This redirect url is neither defined under the .userinrc.json nor in the request payload.'
 			})
 			next()
@@ -116,7 +142,7 @@ const getAuthResponseHandler = ({ strategy, userPortal, redirectUrls, callbackPa
 		if (!successRedirect) {
 			send(res, {
 				code:500,
-				message: defaultErrorMessage,
+				message: `${defaultErrorMessage} (code 005)`,
 				verboseMessage: 'Missing required redirect success url. This redirect url is neither defined under the .userinrc.json nor in the request payload.'
 			})
 			next()
@@ -125,13 +151,13 @@ const getAuthResponseHandler = ({ strategy, userPortal, redirectUrls, callbackPa
 
 		const formatErrorUrl = ({ code, message, verboseMessage, data }) => addErrorToUrl(errorRedirect, { code, message, verboseMessage, data })
 
-		const callbackURL = callbackPathname ? getCallbackUrl(req, callbackPathname) : undefined
+		const callbackURL = callbackPathname ? getCallbackUrl(req, callbackPathname, { redirectsWithQueryParams }) : undefined
 		const handler = passport.authenticate(strategy, { callbackURL }, (err,user) => {
 			// CASE 1 - IdP Failure
 			if (err) {
 				const redirectUrl = formatErrorUrl({
 					code:500, 
-					message: defaultErrorMessage,
+					message: `${defaultErrorMessage} (code 006)`,
 					verboseMessage: err.message
 				})
 				res.redirect(redirectUrl)
